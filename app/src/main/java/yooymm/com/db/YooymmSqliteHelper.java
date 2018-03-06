c68c293e9487325122075cf21b8f2ff19aa872fc
Diminess <1570406018@qq.com>
2018-03-06 14:53:13 +0800

    数据库

---
+++ app/src/main/java/yooymm/com/db/YooymmSqliteHelper.java
@@ -0,0 +1,35 @@
+package yooymm.com.db;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import yooymm.com.util.Constant;
+
+public class YooymmSqliteHelper extends SQLiteOpenHelper {
+
+	
+
+	public YooymmSqliteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	public YooymmSqliteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+		super(context, name, factory, version, errorHandler);
+	}
+
+	public YooymmSqliteHelper(Context context){
+		super(context, Constant.DATABASE_NAME,null,Constant.DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+	}
+}
--- app/src/main/java/yooymm/com/util/Constant.java
+++ app/src/main/java/yooymm/com/util/Constant.java
@@ -10,7 +10,7 @@ package yooymm.com.util;
 
 public class Constant {
 
-    public static final String DATABASE_NAME =  "yooymm.db";
+    public static final String DATABASE_NAME =  "yooymmDB.db";
     public static final int DATABASE_VERSION= 1;
     public static final String TABLE_NAME = "person";
     public static final String _ID = "_id";
